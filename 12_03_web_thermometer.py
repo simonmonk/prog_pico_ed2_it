c138c28f80ae6730b73f7223cf8ce39b23cdfee2
Moreware <35970656+LuigiMorelli@users.noreply.github.com>
2024-05-13 14:19:02 +0200

    Update 12_03_web_thermometer.py
    Traduzione in Italiano

--- 12_03_web_thermometer.py
+++ 12_03_web_thermometer.py
@@ -5,19 +5,19 @@ import mm_wlan
 ssid = 'Network'
 password = 'password'
 
-temp_sensor = ADC(4)
-points_per_volt = 3.3 / 65535
+temp_sensore = ADC(4)
+punti_per_volt = 3.3 / 65535
 
 app = Microdot()  
 mm_wlan.connect_to_network(ssid, password)
 
 def read_temp_c():
-    reading = temp_sensor.read_u16() * points_per_volt
-    temp_c = 27 - (reading - 0.706)/0.001721
+    lettura = temp_sensore.read_u16() * punti_per_volt
+    temp_c = 27 - (lettura - 0.706)/0.001721
     return temp_c
 
 @app.route('/')
 def index(request):
-    return 'Temperature: ' + str(read_temp_c())
+    return 'Temperatura: ' + str(read_temp_c())
 
 app.run(port=80)
